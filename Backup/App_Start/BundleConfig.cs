aee8b8ab9e2f97050e7d93025d9422372c068a17
Chris <cjwahldotcom@gmail.com>
2018-05-20 11:05:17 -0500

    Updates

---
+++ Backup/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace cjwahldotcom
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/bootstrap.bundle.min.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/Content/main.css",
+                      "~/Content/bootstrap/bootstrap.min.css"));
+        }
+    }
+}
---
+++ Backup/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cjwahldotcom.MvcApplication" Language="C#" %>
